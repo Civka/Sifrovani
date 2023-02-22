c328a6f6d1dac310087874c691760198c75bbc74
Civka <daniel.civin@seznam.cz>
2023-02-22 15:10:21 +0100

    Design hotov

--- Sifrovani/Form1.Designer.cs
+++ Sifrovani/Form1.Designer.cs
@@ -28,13 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Base64 = new System.Windows.Forms.TabPage();
+            this.Synchroní = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.Base64.SuspendLayout();
+            this.Synchroní.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Base64);
+            this.tabControl1.Controls.Add(this.Synchroní);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(269, 189);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Base64
+            // 
+            this.Base64.Controls.Add(this.button2);
+            this.Base64.Controls.Add(this.button1);
+            this.Base64.Controls.Add(this.textBox3);
+            this.Base64.Controls.Add(this.textBox2);
+            this.Base64.Controls.Add(this.textBox1);
+            this.Base64.Location = new System.Drawing.Point(4, 22);
+            this.Base64.Name = "Base64";
+            this.Base64.Padding = new System.Windows.Forms.Padding(3);
+            this.Base64.Size = new System.Drawing.Size(261, 163);
+            this.Base64.TabIndex = 0;
+            this.Base64.Text = "Base64";
+            this.Base64.UseVisualStyleBackColor = true;
+            // 
+            // Synchroní
+            // 
+            this.Synchroní.Controls.Add(this.label1);
+            this.Synchroní.Controls.Add(this.textBox7);
+            this.Synchroní.Controls.Add(this.button3);
+            this.Synchroní.Controls.Add(this.textBox6);
+            this.Synchroní.Controls.Add(this.textBox5);
+            this.Synchroní.Controls.Add(this.textBox4);
+            this.Synchroní.Location = new System.Drawing.Point(4, 22);
+            this.Synchroní.Name = "Synchroní";
+            this.Synchroní.Padding = new System.Windows.Forms.Padding(3);
+            this.Synchroní.Size = new System.Drawing.Size(261, 163);
+            this.Synchroní.TabIndex = 1;
+            this.Synchroní.Text = "Synchronní";
+            this.Synchroní.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(249, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 58);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(249, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Šifruj";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Ze souboru";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 6);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(249, 20);
+            this.textBox4.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(6, 32);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(249, 20);
+            this.textBox5.TabIndex = 2;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(6, 58);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(249, 20);
+            this.textBox6.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(184, 84);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 66);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Šifruj";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(18, 108);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Heslo:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.Base64.ResumeLayout(false);
+            this.Base64.PerformLayout();
+            this.Synchroní.ResumeLayout(false);
+            this.Synchroní.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Base64;
+        private System.Windows.Forms.TabPage Synchroní;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
 
